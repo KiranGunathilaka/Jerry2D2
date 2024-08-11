450f803f3470d41f636740b8ef1ec11c29b5aca9
KavishkaJayakody <jayakodykavishka@gmail.com>
2024-08-11 06:55:48 +0530

    added udp receiver

--- src/main.cpp
+++ src/main.cpp
@@ -29,23 +29,24 @@ void loop()
   for (int i=0; i<=20;i++){
       float l = 0;
       float r = 0;
-      for(int j=0; j<40;j++){
-         motors.set_left_motor_percentage(motors.left_feed_forward_percentage(110.0));
-         motors.set_right_motor_percentage(motors.left_feed_forward_percentage(110.0));
-         delay(100);
+      for(int j=0; j<16;j++){
+         motors.set_left_motor_percentage(motors.left_feed_forward_percentage(50+j*30));
+         motors.set_right_motor_percentage(motors.left_feed_forward_percentage(50+j*30));
+         delay(1000);
          encoders.update();
-         l += encoders.leftRPS();
-         r += encoders.rightRPS();
-      }
-      int lavg = l/40;
-      int ravg = r/40;
-      Serial.print(i);
-      Serial.print(" l ");
-      Serial.print(motors.left_feed_forward_percentage(110.0));
-      Serial.print("  r  ");
-      Serial.println(ravg);
-      // Serial.print(" speed ");
-      // Serial.println(encoders.robot_speed());
+         Serial.println(i);
+         Serial.print("  given velocity  ");
+         Serial.print(50+j*30);
+         Serial.print(" actual velocity ");
+         Serial.print(encoders.robot_speed());
+         Serial.print("  motor percentage  ");
+         Serial.println(motors.left_feed_forward_percentage(50+j*30));
+  
+
+         Serial.println(encoders.robotDistance());
+         Serial.println(encoders.robotAngle());
+         
+       }
 
 
       
