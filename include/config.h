521fc016a59d6bbdc5a6aaca2975aab47085f4ab
KiranGunathilaka <kirangunathilaka@gmail.com>
2024-09-16 23:04:57 +0530

    going to add search maze

--- include/config.h
+++ include/config.h
@@ -143,10 +143,7 @@ const int BATTERY_PIN = 4;
 //***************************************************Maze*******************************************************************************
 
 
-#define TARGET1 Location(3,4)
-#define TARGET2 Location(3,4)
-#define TARGET3 Location(3,4)
-#define TARGET4 Location(3,4)
+#define TARGET Location(3,4)
 #define MAZE_WIDTH 8
 #define MAZE_HEIGHT 8
 #define MAZE_CELL_COUNT (MAZE_WIDTH * MAZE_HEIGHT)
